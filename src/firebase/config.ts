222cb26455c35a9fb1821cec4783cabf0aa627a8
gino rojo jorquera <ginorojoj@gmail.com>
2025-11-15 03:01:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5735523812-5acd6",
+  "appId": "1:937036946050:web:2d5607817bd12ea283f29b",
+  "apiKey": "AIzaSyDd6KNgZToIL8gImaSNR4mGwo4UaSU6iw0",
+  "authDomain": "studio-5735523812-5acd6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "937036946050"
+};
