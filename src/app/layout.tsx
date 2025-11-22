709445db87c77929c98b10b56317fb5bbf870d3c
gino rojo jorquera <ginorojoj@gmail.com>
2025-11-22 04:55:13 +0000

    porque cuando despliego en vercel todavia se ve el favicon de firebase y

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/logo.webp" type="image/webp" sizes="any" />
+        <link rel="apple-touch-icon" href="/logo.webp" />
         <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
