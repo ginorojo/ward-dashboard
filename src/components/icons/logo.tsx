a33149d364243194df3414399e0a38e1db83be9a
gino rojo jorquera <ginorojoj@gmail.com>
2025-11-22 04:45:10 +0000

    necesito eliminarle el fondo blanco que tiene la imagen, para que quede

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,14 +1,15 @@
 'use client';
 import Image from 'next/image';
 import React from 'react';
+import { cn } from '@/lib/utils';
 
 const Logo = ({ className }: { className?: string }) => (
-  <div className={className} style={{ width: '100%', height: '100%', position: 'relative' }}>
+  <div className={cn('relative rounded-full overflow-hidden', className)}>
     <Image
       src="/logo.webp"
       alt="Ward Dashboard Logo"
       layout="fill"
-      objectFit="contain"
+      objectFit="cover"
     />
   </div>
 );
